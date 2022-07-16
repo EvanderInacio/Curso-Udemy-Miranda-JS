3f0e8a3b12601e96f041dd1b352d56f39ce54554
Evander Inacio <evander.20112@hotmail.com>
2022-07-15 22:06:41 -0300

    feat: :building_construction: error

--- src/section-3/aula-64/aula64.js
+++ src/section-3/aula-64/aula64.js
@@ -1,27 +1,26 @@
 try {
- // executado quando não há erros
-} catch(e) {
- // executado quando há erros 
+  // executado quando não há erros
+} catch (e) {
+  // executado quando há erros
 } finally {
   // sempre
 }
 
-
-function horas(data){
-  if(data && !(data instanceof Date)) {
+function horas(data) {
+  if (data && !(data instanceof Date)) {
     throw new TypeError('Esperando instância de Date.')
-  } 
+  }
 
-  if (!data ) {
+  if (!data) {
     data = new Date()
   }
 
-  return data.toLocaleTimeString('pt-BR' , {
+  return data.toLocaleTimeString('pt-BR', {
     hour: '2-digit',
     minute: '2-digit',
     second: '2-digit',
     hour12: false
-  } )
+  })
 }
 
 try {
